abc416213cdbb2fb6ace00a782294abe73aec747
Jorge Viscal <jviscal@vt.edu>
2025-11-07 21:34:02 -0500

    route testing 5

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,18 +1,26 @@
 import { Outlet } from "react-router";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { JobProvider } from "../lib/jobStore";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string;
 
 export default function Layout() {
 	return (
-		<div className="min-h-screen flex flex-col">
-			<Header />
-			{/* main content area gets the light-green background so header/nav remain distinct */}
-			<main className="flex-1 bg-white">
-				<div className="container mx-auto p-4 pt-6 min-h-0">
-					<Outlet />
+		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+			<JobProvider>
+				<div className="min-h-screen flex flex-col">
+					<Header />
+					{/* main content area gets the light-green background so header/nav remain distinct */}
+					<main className="flex-1 bg-white">
+						<div className="container mx-auto p-4 pt-6 min-h-0">
+							<Outlet />
+						</div>
+					</main>
+					<Footer />
 				</div>
-			</main>
-			<Footer />
-		</div>
+			</JobProvider>
+		</ClerkProvider>
 	);
 }
